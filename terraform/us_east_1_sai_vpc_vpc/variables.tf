9cccf12fa8e69109619fa057d96113fa4aaaf80b
Sai Prashanth K <114280375+PrashanthSai-K@users.noreply.github.com>
2026-04-21 11:23:47 +0530

    chore: add Stratum-generated Terraform modules

---
+++ terraform/us_east_1_sai_vpc_vpc/variables.tf
@@ -0,0 +1,10 @@
+variable "enable_dns_hostnames" {
+  type = bool
+  description = "Enable DNS hostnames in the VPC"
+}
+
+variable "enable_dns_support" {
+  type = bool
+  description = "Enable DNS support in the VPC"
+}
+
